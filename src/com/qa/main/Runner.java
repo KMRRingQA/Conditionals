e0deadb0292838fa61139be60bddcc1e320d3f98
Korbinian <kmr.ring@outlook.com>
2020-03-24 23:45:31 +0000

    more values displayed & added capture to show working program

--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -3,6 +3,7 @@ package com.qa.main;
 public class Runner {
 
 	public static void main(String[] args) {
+		System.out.println("Beginning of Q1:\n");
 		Q1_Calculator calculator = new Q1_Calculator();
 		System.out.println(calculator.calculate(1,2,true));
 		System.out.println(calculator.calculate(3,3,false));
@@ -17,6 +18,9 @@ public class Runner {
 		
 		Q3_NoEvens calculator2 = new Q3_NoEvens();
 		System.out.println(calculator2.calculate(1,4,true));
+		System.out.println(calculator2.calculate(1,3,true));
+		System.out.println(calculator2.calculate(3,3,false));
+		System.out.println(calculator2.calculate(3,4,false));
 
 	}
 
